19cd3e666eaffa47bf8390df557404613d1e98d4
dr3y <ashur@caltech.edu>
2019-10-03 15:17:40 -0700

    update file path

--- runsensor/sensorDaemon.py
+++ runsensor/sensorDaemon.py
@@ -9,7 +9,8 @@ from TCS34725 import TCS34725
 
 ledpin = 7 #all the LEDs are the same pin
 delay = 25 #read every 25 seconds
-statuspath = os.path.join('..','data','statusfile.txt')
+datapath = os.path.join('..','data')
+statuspath = os.path.join(datapath,'statusfile.txt')
 
 GPIO.setmode(GPIO.BOARD) #set the pin numbering
 GPIO.setup(ledpin,GPIO.OUT,initial=GPIO.LOW)
@@ -23,16 +24,16 @@ while True:
             current_starttime = float(lsplit[2])
             try:
                 #check if the file is there!
-                fle = open(current_filename,'r')
+                fle = open(os.path.join(datapath,current_filename),'r')
                 firstline = fle.readlines()[0].split(",")
                 if(firstline[0]=="time"):
                     pass
                 fle.close()
             except FileNotFoundError:
                 #if the file isn't there, then make it!
-                initFile(current_filename)
+                initFile(os.path.join(datapath,current_filename))
             sensor = TCS34725(current_i2caddress)
             #senstime = time.time()-current_starttime
             data = takeReading(sensor)
-            updateDict({},current_starttime,data,current_filename)
+            updateDict({},current_starttime,data,os.path.join(datapath,current_filename))
     time.sleep(delay)

e76a0eb1d865ab864c326fb218aa90284dbe4cef
dr3y <ashur@caltech.edu>
2019-10-09 21:25:33 -0700

    i2c typos

--- runsensor/TCA9548A.py
+++ runsensor/TCA9548A.py
@@ -12,7 +12,7 @@ class TCA9548A():
         self.i2caddress = address
     def tcaselect(self,portnum):
         """tells the multiplexer to select a port"""
-        bus.write_byte_data(self.i2caddress,1<<portnum)
+        bus.write_byte_data(self.i2caddress,1,1<<portnum)
     def scan(self):
         """scans through all multiplexer channels and all i2c addresses.
         If the data send doesn't fail, it means there's a device there."""

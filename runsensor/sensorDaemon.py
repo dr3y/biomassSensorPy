e476a3ee973de6696662c321b17b271464de49d3
dr3y <ashur@caltech.edu>
2019-10-10 13:07:06 -0700

    testing bqplot and auto-detection of sensors

--- runsensor/sensorDaemon.py
+++ runsensor/sensorDaemon.py
@@ -17,7 +17,6 @@ GPIO.setmode(GPIO.BOARD) #set the pin numbering
 GPIO.setup(ledpin,GPIO.OUT,initial=GPIO.LOW)
 while True:
     with open(statuspath,"r") as statfle:
-
         tomeasure = []
         lastsensor = None
         multiplexer = TCA9548A()
@@ -27,7 +26,7 @@ while True:
             current_filename = lsplit[0]
             current_i2caddress = int(lsplit[1])
             current_starttime = float(lsplit[2])
-            tomeasure+=[[current_filename,current_i2caddress,current_starttime,None]]
+
             try:
                 #check if the file is there!
                 fle = open(os.path.join(datapath,current_filename),'r')
@@ -38,26 +37,39 @@ while True:
             except FileNotFoundError:
                 #if the file isn't there, then make it!
                 initFile(os.path.join(datapath,current_filename))
-            multiplexer.tcaselect(current_i2caddress)
-            sensor = TCS34725()
-            lastsensor = sensor
-        GPIO.output(ledpin,GPIO.HIGH) #LED on
-        time.sleep(1.4)
-        for sensor in tomeasure:
-            #take light measurements for everything
-            multiplexer.tcaselect(sensor[1])
-            data = takeBGReading(lastsensor)
-            sensor[3] = data
-        GPIO.output(ledpin,GPIO.LOW) #LED off
-        time.sleep(1.4)
-        for sensor in tomeasure:
-            #take dark measurements for everything
-            multiplexer.tcaselect(sensor[1])
-            lum_ctrl = takeBGReading(lastsensor)
-            sensor[3]['ctrl_r'] = lum_ctrl['r']
-            sensor[3]['ctrl_g'] = lum_ctrl['g']
-            sensor[3]['ctrl_b'] = lum_ctrl['b']
-            sensor[3]['ctrl_c'] = lum_ctrl['c']
-            #now, save everything
-            updateDict({},sensor[2],sensor[3],os.path.join(datapath,sensor[0]))
+            try:
+                multiplexer.tcaselect(current_i2caddress)
+            except OSError:
+                #this happens if the multiplexer isn't plugged in
+                #just keep trying until it works
+                continue
+            try:
+                sensor = TCS34725()
+                lastsensor = sensor
+            except OSError:
+                #this happens if the sensor isn't connected
+                continue
+            #if we made it all the way down here, everything connected!
+            tomeasure+=[[current_filename,current_i2caddress,current_starttime,None]]
+        if(len(tomeasure)>0):
+            #only do this stuff if there's something to measure
+            GPIO.output(ledpin,GPIO.HIGH) #LED on
+            time.sleep(1.4)
+            for sensor in tomeasure:
+                #take light measurements for everything
+                multiplexer.tcaselect(sensor[1])
+                data = takeBGReading(lastsensor)
+                sensor[3] = data
+            GPIO.output(ledpin,GPIO.LOW) #LED off
+            time.sleep(1.4)
+            for sensor in tomeasure:
+                #take dark measurements for everything
+                multiplexer.tcaselect(sensor[1])
+                lum_ctrl = takeBGReading(lastsensor)
+                sensor[3]['ctrl_r'] = lum_ctrl['r']
+                sensor[3]['ctrl_g'] = lum_ctrl['g']
+                sensor[3]['ctrl_b'] = lum_ctrl['b']
+                sensor[3]['ctrl_c'] = lum_ctrl['c']
+                #now, save everything
+                updateDict({},sensor[2],sensor[3],os.path.join(datapath,sensor[0]))
     time.sleep(delay)

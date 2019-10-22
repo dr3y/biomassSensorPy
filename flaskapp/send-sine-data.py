c5a12b370ea88a3344da268d9dc2480b91412bdb
dr3y <ashur@caltech.edu>
2019-10-21 18:27:08 -0700

    live updates v1

--- flaskapp/send-sine-data.py
+++ flaskapp/send-sine-data.py
@@ -3,6 +3,7 @@ import json
 import time
 import asyncio
 import websockets
+import os
 
 _GOODBYE_MESSAGE = u'Goodbye'
 
@@ -15,12 +16,65 @@ graph_size = 100
 samples = 0
 tic = time.time()
 
-def get_graph_data():
-
+def get_graph_data(fname):
+    #print("statusfile is "+fname)
     global counter,data_size,graph_size,x,y
     global samples,tic
+    curtime = time.time()
+    chanlist = []
+    with open(fname,'r') as statusfile:
+        channelslist = {}
+        for line in statusfile:
+            lspl = [ a.strip() for a in line.split(',')]
 
+            channame = int(lspl[1]) #which channel is it? 0-6
+            chanlist+=[channame]
+            chanfile = lspl[0] #filename. Can be empty
+            connected = int(lspl[3]) #is this channel connected? 1 or 0
+            startbut = 0
+            endbut = 0
+            visible = True
+            #possibile scenarios:
+            if(connected):
+                print("channel "+str(channame)+" is connected")
+                if(chanfile==''):
+                    #in this case we are connected but havent started
+                    #recording
+                    startbut = 0 #this means "start" is available
+                    endbut = 1 #this means "end" is not shown
+                    x=(0)
+                    y=(0)
+                else:
+                    #in this case we have been recording
+                    startbut = 1 #this means start is in "pause"
+                    endbut = 0 #end is enabled
+                    x = []
+                    y = []
+                    if(chanfile[0]=="."):
+                        chanfile = "."+chanfile
+                    with open(chanfile, 'r') as datafile:
+                        for dataline in datafile:
+                            if('time' in dataline):
+                                continue
+                            datasplit = dataline.split(",")
+                            newx = float(datasplit[0])
+                            newy = float(datasplit[4])-float(datasplit[8])
+                            x+=[newx]
+                            y+=[newy]
+                    x = tuple(x)
+                    y = tuple(y)
+            else:
+                #this means we are not connected
+                visible = False
+                startbut = 2 #this means startbut is not visible
+                endbut = 1 #this means endbut is not visible
+            channelslist[str(channame)] = {"data":{"x":x,"y":y},\
+                                    "buttons":{"start":startbut,"end":endbut},\
+                                    "visible":visible}
+    channelslist["channels"]=[str(a) for a in chanlist]
+    return json.dumps(channelslist)
     #Calculate FPS
+    '''
     samples += 1
     if (time.time() - tic) > 2:
         print("FPS is : "+str(samples /(time.time() - tic)))
@@ -36,10 +90,11 @@ def get_graph_data():
         'y':y[counter:counter+graph_size]
     })
     return graph_to_send
-
+    #'''
+#print(get_graph_data(os.path.join("..","data","statusfile.txt")))
 async def hello(websocket,path):
     while True:
-        await websocket.send(get_graph_data())
+        await websocket.send(get_graph_data(os.path.join("..","data","statusfile.txt")))
         await asyncio.sleep(.2)
     #async with websockets.connect(uri) as websocket:
     #    await websocket.send(get_graph_data())
--- flaskapp/testapp.py
+++ flaskapp/testapp.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import json
 import plotly
-import pandas as pd
+#import pandas as pd
 import numpy as np
 import plotly.graph_objs as go
 
@@ -12,51 +12,21 @@ app.debug = True
 
 @app.route('/')
 def index():
-    rng = pd.date_range('1/1/2011', periods=750, freq='H')
-    ts = pd.Series(np.random.randn(len(rng)), index=rng)
 
     graphs = [
         dict(
             data=[
                 dict(
-                    x=[1, 2, 3],
-                    y=[10, 20, 30],
-                    type='scatter'
-                ),
-            ],
-            layout=dict(
-                title='first graph',
-                margin={'l':10,'r':10}
-            )
-        ),
-
-        dict(
-            data=[
-                dict(
-                    x=[1, 3, 5],
-                    y=[10, 50, 30],
-                    type='bar'
-                ),
-            ],
-            layout=dict(
-                title='second graph',
-                margin={'l':10,'r':10}
-            )
-        ),
-
-        dict(
-            data=[
-                dict(
-                    x=ts.index,  # Can use the pandas data structures directly
-                    y=ts
+                    x=(0,),
+                    y=(0,)
                 )
             ],
             layout=dict(
-                title='third graph',
+                title='graph {}'.format(a),
                 margin={'l':10,'r':10}
             )
         )
-    ]*3
+     for a in range(9)]
 
     # Add "ids" to each of the graphs to pass up to the client
     # for templating
